42c697a851f138e4374a2a04ec9f7b616109ebce
Bus Tracking Developer <developer@bustracking.com>
2026-07-05 21:03:40 +0530

    Added fc8 program to convert uppercase to lowercase and vice versa

---
+++ FlowControlStatments/fc8.java
@@ -0,0 +1,11 @@
+public class fc8 {
+    public static void main(String[] args) {
+
+        char ch = 'A';
+
+        if (ch >= 'a' && ch <= 'z')
+            System.out.println(ch + "->" + Character.toUpperCase(ch));
+        else if (ch >= 'A' && ch <= 'Z')
+            System.out.println(ch + "->" + Character.toLowerCase(ch));
+    }
+}
