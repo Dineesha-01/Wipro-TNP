31bd1ffc2daefdebd28d714c574dfa0ad5e7f538
Bus Tracking Developer <developer@bustracking.com>
2026-07-05 20:26:16 +0530

    Added fc3 program to check whether a number is odd or even

---
+++ FlowControlStatments/fc3.java
@@ -0,0 +1,10 @@
+public class fc3 {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+
+        if (n % 2 == 0)
+            System.out.println(n + " is even");
+        else
+            System.out.println(n + " is odd");
+    }
+}
