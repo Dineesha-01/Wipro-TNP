66c009f217d42983418a36774a9225a85ac62216
Bus Tracking Developer <developer@bustracking.com>
2026-07-05 20:14:26 +0530

    Added fc1 program for positive negative zero

---
+++ FlowControlStatments/fc1.java
@@ -0,0 +1,12 @@
+public class fc1 {
+        public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+
+        if (n > 0)
+            System.out.println(n + " is positive");
+        else if (n < 0)
+            System.out.println(n + " is negative");
+        else
+            System.out.println(n + " is zero");
+    }
+}
