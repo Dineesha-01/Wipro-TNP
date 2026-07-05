ae9ace8a546c690937ebd646448ce6676acf96a8
Bus Tracking Developer <developer@bustracking.com>
2026-07-05 22:42:12 +0530

    Added arr14 program to find biggest number in 3x3 matrix

---
+++ Arrays/arr14.java
@@ -0,0 +1,35 @@
+public class arr14 {
+    public static void main(String[] args) {
+
+        if (args.length != 9) {
+            System.out.println("Please enter 9 integer numbers");
+            return;
+        }
+        int[][] arr = new int[3][3];
+        int index = 0;
+        int max;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = Integer.parseInt(args[index++]);
+            }
+        }
+        System.out.println("The given array is :");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        max = arr[0][0];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                }
+            }
+        }
+
+        System.out.println("The biggest number in the given array is " + max);
+    }
+}
