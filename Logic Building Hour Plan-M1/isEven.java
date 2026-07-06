67ebdf04e60c38d617eda57db06e84142a28b15c
Dineesha-01 <23BQ1A1214@vvit.net>
2026-07-06 22:14:08 +0530

    Refactor isEven function into Solution class

--- Logic Building Hour Plan-M1/isEven.java
+++ Logic Building Hour Plan-M1/isEven.java
@@ -1,4 +1,5 @@
-int isEven(int input1)
-{
-    return (input1 % 2 == 0) ? 2 : 1;
+public class Solution {
+    public static int isEven(int input1) {
+        return (input1 % 2 == 0) ? 2 : 1;
+    }
 }
