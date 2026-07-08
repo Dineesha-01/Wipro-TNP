54d800cd8da5a447e61e17a3288b29aa6535ce73
Dineesha-01 <23BQ1A1214@vvit.net>
2026-07-08 22:00:10 +0530

    Add Apple class that extends Fruit and overrides eat method

---
+++ OOPS/Overriding/Apple.java
@@ -0,0 +1,11 @@
+public class Apple extends Fruit {
+
+    public Apple(String name, String taste, String size) {
+        super(name, taste, size);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(name + " tastes Sweet");
+    }
+}
